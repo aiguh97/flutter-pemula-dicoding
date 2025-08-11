e38558b5d47348bc7ed0a378267d7aaf1e4b8b60
minguh <aiguh97@gmail.com>
2025-08-12 06:13:10 +0700

    2_stateless_widget_vs_statefull_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pemula_dicoding/biggerText.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,10 +17,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: false, // Pastikan M2 dipakai agar AppBar ikut warna
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hello, world!')),
-        body: const Center(child: Text('Hello, world!')),
+      home: const Scaffold(
+        body: Center(
+          child: BiggerText(
+            text: "Hello world!",
+          ), // Ubah widget Heading ke PerubahanText
+        ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
     );
   }
 }
